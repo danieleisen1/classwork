0c43c3e159fc5266ccdcad37adeeaabe98628b53
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-10-23 15:31:46 -0400

    asdf

---
+++ Eisen/src/caveExplorer/caveExplorer.java
@@ -0,0 +1,12 @@
+package caveExplorer;
+
+public class caveExplorer {
+	public static CaveRoom[][] caves;
+	public static Scanner in;
+	public static CaveRoom currentRoom;
+	
+	public caveExplorer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
